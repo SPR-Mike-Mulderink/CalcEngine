7c7bcf163937a16e823b281cf11349cec81dc4a8
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-10-03 11:21:42 -0500

    Added Constructors to MathEquation Class

--- src/com/mulderink1/calcengine/Main.java
+++ src/com/mulderink1/calcengine/Main.java
@@ -7,24 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0, 17.0, 's');
-        equations[3] = create(11.0, 3.0, 'm');
+        equations[0] = new MathEquation( 'd', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0, 17.0);
+        equations[3] = new MathEquation('m', 11.0, 3.0);
 
         for (MathEquation equation : equations) {
             equation.execute();
             System.out.printf("Result = %s \n", equation.getResult());
 
         }
-    }
-
-    public static MathEquation create(double leftVal, double rightVal, char opCode) {
-        MathEquation equation = new MathEquation();
-        equation.setLeftVal(leftVal);
-        equation.setRightVal(rightVal);
-        equation.setOpCode(opCode);
-
-        return equation;
     }
 }
